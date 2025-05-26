df3088ed901f61e8e6f742b2e01b050ca26c084b
alxb <alx.b2la@gmail.com>
2025-05-26 21:02:02 +0300

    * Add booking and request models

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.request;
+package ru.practicum.shareit.item;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -6,24 +6,27 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
-import java.time.LocalDateTime;
-
 /**
- * TODO Sprint add-item-requests.
+ * Класс—модель данных приложения, дающий описание объекту Item.
  */
 
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PUBLIC)
-@Builder(toBuilder = true)
-public class ItemRequest {
+@Builder
+public class Item {
     private Long id;
+
+    @NotBlank
+    private String name;
 
     @NotBlank
     private String description;
 
     @NotNull
-    private User requester;
-    private LocalDateTime created;
+    private Boolean available;
+    private User owner;
+    private ItemRequest request;
 }
